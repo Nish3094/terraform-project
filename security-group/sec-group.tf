2bb3c4f5af312e3b083922496e0ecff33bc12a3a
Nish3094 <marianishanthd@gmail.com>
2021-07-03 17:46:36 +0530

    Add iterator for better readability

--- security-group/sec-group.tf
+++ security-group/sec-group.tf
@@ -34,9 +34,10 @@ resource "aws_security_group" "demo_sg" {
 
   dynamic ingress {
     for_each = var.sg_ports
+    iterator = port
     content {
-      from_port   = ingress.value
-      to_port     = ingress.value
+      from_port   = port.value
+      to_port     = port.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
