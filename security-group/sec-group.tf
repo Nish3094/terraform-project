736aaefaf26461bb98c54683d7648b6b19b6d5e5
Nish3094 <39150263+Nish3094@users.noreply.github.com>
2021-07-03 17:58:08 +0530

    Update sec-group.tf

--- security-group/sec-group.tf
+++ security-group/sec-group.tf
@@ -42,4 +42,16 @@ resource "aws_security_group" "demo_sg" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
+  
+  
+ dynamic egress {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 }
